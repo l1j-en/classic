aa37defd0f662ccc714aafece70542f367aed6cd
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2008-11-02 01:42:50 +0000

    File management in relation to the completion of update 35 and the beginning of update 36.

---
+++ db/update_36-pending.sql
@@ -0,0 +1,7 @@
+-- IMPORTANT: THIS UPDATE IS NOT YET FINISHED
+
+-- Queries that are pending an update should be placed here
+-- this allows them to be verified as working together before being
+-- migrated to the DML scripts
+
+-- update 36
