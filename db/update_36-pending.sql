30febe149efd50f9fbcb188b92ceb2a01612296e
tricid <tricid@l1j.org>
2008-11-03 15:45:15 +0000

    Set all wand drops to 1. These were causing issues with certain bosses

--- db/update_36-pending.sql
+++ db/update_36-pending.sql
@@ -5,3 +5,6 @@
 -- migrated to the DML scripts
 
 -- update 36
+
+-- Set all wand drops to 1. Was causing issues with certain bosses
+UPDATE droplist SET min = 1, MAX = 1 WHERE itemid = 40006 OR itemid = 40007 OR itemid = 40008 OR itemid = 40009 OR itemid = 140006 OR itemid = 140008;
