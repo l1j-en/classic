ebbfd18e2b900a7ab5f0f39cf45ec54483d9664c
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2008-11-04 09:05:41 +0000

    Fixed Issue #39: Exhausted lizardman warrior spawn location

--- db/update_36-pending.sql
+++ db/update_36-pending.sql
@@ -8,3 +8,6 @@
 
 -- Set all wand drops to 1. Was causing issues with certain bosses
 UPDATE droplist SET min = 1, MAX = 1 WHERE itemid = 40006 OR itemid = 40007 OR itemid = 40008 OR itemid = 40009 OR itemid = 140006 OR itemid = 140008;
+
+-- correct exhausted lizardman warrior spawn location
+update spawnlist_npc set locx = 32790, locy = 32975, randomx = 0, randomy = 0 where npc_templateid = 71075;
