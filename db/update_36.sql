814dbc0b1ea5a2feb4278422bc599e966ea38a7d
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2008-11-22 23:41:53 +0000

    Removed warning msg from update_36.sql.

--- db/update_36.sql
+++ db/update_36.sql
@@ -1,9 +1,3 @@
--- IMPORTANT: THIS UPDATE IS NOT YET FINISHED
-
--- Queries that are pending an update should be placed here
--- this allows them to be verified as working together before being
--- migrated to the DML scripts
-
 -- update 36
 
 -- Set all wand drops to 1. Was causing issues with certain bosses
