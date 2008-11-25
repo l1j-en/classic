80ae85f9e37a7dcea60ebbc5a2796221b5e6d175
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2008-11-25 09:03:50 +0000

    Fixed Issue #54: NPC Becky.

--- db/update_37-pending.sql
+++ db/update_37-pending.sql
@@ -9,3 +9,6 @@
 -- swap name/nameids of Coin of Reputation and Coin of Bravery
 update etcitem set name = 'Coin of Bravery', name_id = '$3059' where item_id = 40733;
 update etcitem set name = 'Coin of Reputation', name_id = '$3056' where item_id = 40735;
+
+-- correct name of npc becky
+update npc set name = 'Becky', nameid = 'Becky' where npcid = 71191;
