6bab0f24223457740024c44fe406a537ff0157b2
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2009-01-26 14:37:53 +0000

    Fixed cockatrice eye laser attack.

--- db/update_040-pending.sql
+++ db/update_040-pending.sql
@@ -5,3 +5,8 @@
 -- be removed.
 
 -- update 40
+
+-- Restored cockatrice eye laser attack
+INSERT INTO `skills` VALUES ('12001', 'Cockatrice Laser Attack', '0', '0', '10', '0', '0', '0', '0', '0', 'attack', '3', '50', '5', '5', '0', '0', '0', '64', '0', '6', '0', '0', '0', '0', '19', '1054', '0', '0', '0');
+update mobskill set skillid = 12001 where mobid = 45361 and actno = 0;
+update skills set name = 'unknown' where  skill_id = 10091;
