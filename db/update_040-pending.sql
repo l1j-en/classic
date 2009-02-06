1ae8ab92da0d2d3f3b72ce0449653ee99274b88a
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2009-02-06 06:09:42 +0000

    Fixed DM names.

--- db/update_040-pending.sql
+++ db/update_040-pending.sql
@@ -10,3 +10,10 @@
 INSERT INTO `skills` VALUES ('12001', 'Cockatrice Laser Attack', '0', '0', '10', '0', '0', '0', '0', '0', 'attack', '3', '50', '5', '5', '0', '0', '0', '64', '0', '6', '0', '0', '0', '0', '19', '1054', '0', '0', '0');
 update mobskill set skillid = 12001 where mobid = 45361 and actno = 0;
 update skills set name = 'unknown' where  skill_id = 10091;
+
+-- Fix DM names
+update ub_settings set ub_name = 'Giran' where ub_id = 1;
+update ub_settings set ub_name = 'Werldern' where ub_id = 2;
+update ub_settings set ub_name = 'Gludio' where ub_id = 3;
+update ub_settings set ub_name = 'TI' where ub_id = 4;
+update ub_settings set ub_name = 'SKT' where ub_id = 5;
