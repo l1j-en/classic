5adf51d00db2ef5f232dca445ce266d7231b6be4
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2009-03-01 22:45:10 +0000

    Adjusted heine croc and alligator drops.

--- db/update_042-pending.sql
+++ db/update_042-pending.sql
@@ -13,4 +13,15 @@ update droplist set itemid = 40524, chance = 10000 where itemid = 40678 and mobi
 update etcitem set locx = 32675, locy = 32861, mapid = 69 where item_id = 40101;
 update etcitem set locx = 32816, locy = 32735, mapid = 68 where item_id = 40082;
 
+-- increase croc drops
+-- brave (was 50000)
+update droplist set chance = 120000 where itemid = 40014 and mobid = 45338;
+-- emerald (was 50000)
+update droplist set chance = 120000 where itemid = 40047 and mobid = 45338;
+-- sb:c-l (was 10000)
+update droplist set chance = 35000 where itemid = 40179 and mobid = 45338;
+
+-- increase alligator emerald droprate (was 50000)
+update droplist set chance = 75000 where itemid = 40047 and mobid = 45101;
+
 
