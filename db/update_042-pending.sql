3b4a2156cb4eea435cb9c93d9b1af93e61a14a08
tricid <tricid@l1j.org>
2009-03-05 16:00:46 +0000

    make some locations non markable

--- db/update_042-pending.sql
+++ db/update_042-pending.sql
@@ -24,4 +24,5 @@ update droplist set chance = 35000 where itemid = 40179 and mobid = 45338;
 -- increase alligator emerald droprate (was 50000)
 update droplist set chance = 75000 where itemid = 40047 and mobid = 45101;
 
-
+-- I dont think these should be markable - lasta - underground passage - upper PI - some PI dungeon areas
+update mapids set markable = 0 where mapid = 490 or mapid = 492 or mapid = 494 or mapid = 480 or mapid = 496 or mapid = 481 or mapid = 482 or mapid = 483 or mapid = 484 or mapid = 491 or mapid = 493 or mapid = 495;
