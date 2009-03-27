572987d8cf6584855ab3efefc13c3acae1516e7e
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2009-03-27 20:03:02 +0000

    Fixed all remaining duplicate shop item entries. Minor adjustments to MR ring droprates.

--- db/update_043-pending.sql
+++ db/update_043-pending.sql
@@ -19,3 +19,15 @@ INSERT INTO spawnlist VALUES
 (801500411, "Degenerated Beholder", 5, 45086, 0, 32934, 32811, 0, 0, 32888, 32788, 32959, 32895, 5, 20, 20, 86, 0, 0, 0, 0),
 (801500412, "Cursed Bugbear", 5, 45085, 0, 32901, 32736, 0, 0, 32880, 32704, 32959, 32772, 5, 20, 20, 86, 0, 0, 0, 0);
 
+-- 5MR ring droprate rebalance
+update droplist set chance = chance - 500 where itemid = 20303;
+
+-- 10MR ring droprate rebalance on 5xF
+update droplist set chance = 400 where itemid = 20280 and mobid in (45506, 45524, 45541);
+
+-- remove all duplicate shop items in sell lists
+delete from shop where npc_id = 70014 and item_id = 40089 and order_id = 0;
+delete from shop where npc_id = 70079 and item_id = 40079 and order_id = 0;
+delete from shop where npc_id = 70079 and item_id = 40089 and order_id = 0;
+
+
