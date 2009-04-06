1c0c0ae6be0c5db12a21bdec6699c8fb7a8f4a36
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2009-04-06 16:01:05 +0000

    Fixes for dark elf apprentice and several slight drop modifications.

--- db/update_045-pending.sql
+++ db/update_045-pending.sql
@@ -9,3 +9,49 @@
 
 -- Name fix
 update npc set name = "Ghost Soldier Full of Resentment" where npcid = 45913 OR npcid = 45912;
+
+-- Adjust dark elf apprentice mobskill leverage to be slightly less than dark elf wizard.
+update mobskill set leverage = 4 where mobid = 45256 and actno = 0;
+update mobskill set leverage = 8 where mobid = 45256 and actno = 1;
+
+-- Adjust trirange on dark elf apprentice CoC to be like dark elf wizard
+update mobskill set trirange = -6 where mobid = 45256 and actno = 0;
+
+-- Adjust cast freq on dark elf apprentice to be like dark elf wizard
+update mobskill set trirnd = 40 where mobid = 45256 and actno = 0;
+update mobskill set trirnd = 30 where mobid = 45256 and actno = 1;
+
+-- Increase dark elf wizard CoC leverage (was 4).
+update mobskill set leverage = 5 where mobid = 45447 and actno = 0;
+
+-- Slight increase in bear meat droprate from dire bear (was 50000)
+update droplist set chance = 75000 where mobid = 45252 and itemid = 41275;
+
+-- Modify some phoenix droprates
+update droplist set chance = 15000 where itemid = 20281 and mobid = 45617;
+update droplist set chance = 25000 where itemid = 20304 and mobid = 45617;
+
+-- Modify some TQD droprates
+update droplist set chance = 12000 where itemid = 40052 and mobid = 45464;
+update droplist set chance = 7500 where itemid = 40052 and mobid = 45522;
+update droplist set chance = 18000 where itemid = 40052 and mobid = 45529;
+update droplist set chance = 7000 where itemid = 40052 and mobid = 45530;
+update droplist set chance = 8000 where itemid = 40052 and mobid = 45537;
+update droplist set chance = 30000 where itemid = 40052 and mobid = 45583;
+
+-- Modify some int ammy droprates
+update droplist set chance = 12000 where itemid = 20266 and mobid = 45609;
+update droplist set chance = 550 where itemid = 20266 and mobid = 45481;
+update droplist set chance = 17500 where itemid = 20266 and mobid = 45649;
+
+-- ROTC droprate modifications: increase from DK, decraese succ queen
+update droplist set chance = 1500 where itemid = 20288 and mobid = 45601;
+update droplist set chance = 300 where itemid = 20288 and mobid = 45451;
+update droplist set chance = 500 where itemid = 20288 and mobid = 45450;
+
+-- Modify titan belt droprates
+update droplist set chance = 4500 where itemid = 20320 and mobid = 45610;
+update droplist set chance = 15000 where itemid = 20320 and mobid = 45316;
+
+-- Slight reduction in Giran AG HP regen (was 1500)
+update npc set hpr = 1200 where npcid = 45316;
