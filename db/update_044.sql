4ace6312770ed6f68069bf0238eff067f0626d2d
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2009-04-06 14:13:22 +0000

    Closed update 44. Started update 45.

--- db/update_044.sql
+++ db/update_044.sql
@@ -1,9 +1,3 @@
--- IMPORTANT: THIS UPDATE IS NOT YET FINISHED
-
--- Queries that are pending an update should be placed here. This allows them 
--- to be verified as working together. Once complete, the -pending suffix will
--- be removed.
-
 -- update 44
 
 -- database changes to match the latest merge
---
+++ db/update_045-pending.sql
@@ -0,0 +1,11 @@
+
+-- IMPORTANT: THIS UPDATE IS NOT YET FINISHED
+
+-- Queries that are pending an update should be placed here. This allows them 
+-- to be verified as working together. Once complete, the -pending suffix will
+-- be removed.
+
+-- update 45
+
+-- Name fix
+update npc set name = "Ghost Soldier Full of Resentment" where npcid = 45913 OR npcid = 45912;
