50385e78a8357e368b470a4d1155d2c5e6a1b0eb
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2009-12-07 21:13:14 +0000

    Some archive cleanup.

--- db/archive/milestone_5/update_049.sql
+++ db/archive/milestone_5/update_049.sql
@@ -1,10 +1,4 @@
 
--- IMPORTANT: THIS UPDATE IS NOT YET FINISHED
-
--- Queries that are pending an update should be placed here. This allows them 
--- to be verified as working together. Once complete, the -pending suffix will
--- be removed.
-
 -- update 49
 
 # Setup droplist for Gludio Cemetary Ghosts Quest
