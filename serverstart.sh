72e7104b2cb8c86163715cf2e0c8c3ea9fbf9204
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2010-04-09 14:25:21 +0000

    Updated mysql-connector-java lib.

--- serverstart.sh
+++ serverstart.sh
@@ -7,7 +7,7 @@
 while :; do 
   [ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
   [ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log" 
-java -Xms1024m -Xmx1024m -cp l1jen.jar:lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.7-bin.jar:lib/javolution.jar l1j.server.Server > log/stdout.log 2>&1
+java -Xms1024m -Xmx1024m -cp l1jen.jar:lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.12-bin.jar:lib/javolution.jar l1j.server.Server > log/stdout.log 2>&1
  [ $? -ne 2 ] && break 
 #       /etc/init.d/mysql restart 
          sleep 10 
