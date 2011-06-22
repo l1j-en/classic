0c5736597cb241ef17efbb78b73e1616cf86471d
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-06-22 23:32:43 +0000

    Corrected enchant scroll droprate in HV/SI dungeon.

--- db/update_057-pending.sql
+++ db/update_057-pending.sql
@@ -5,3 +5,12 @@
 -- Queries that are pending an update should be placed here. This allows them
 -- to be verified as working together. Once complete, the -pending suffix will
 -- be removed.
+
+-- Address issue 143.
+update droplist set chance = 9000 where mobid = 45086 and itemid = 40087;
+update droplist set chance = 9000 where mobid = 45085 and itemid = 40074;
+update droplist set chance = 7000 where mobid = 45085 and itemid = 40087;
+update droplist set chance = 9000 where mobid = 45075 and itemid = 40074;
+update droplist set chance = 9000 where mobid = 45074 and itemid = 40087;
+update droplist set chance = 9000 where mobid = 45076 and itemid = 40087;
+update droplist set chance = 7500 where mobid = 45086 and itemid = 240087;
