f112119706a0aae8a38a9fcac27f604e061fbba8
tricid <tricid@l1j.org>
2011-06-22 21:54:56 +0000

    added notes to 56 and 57.  Don't edit update 56 directly anymore.  Changes need to be done in 57 with alter/drop statements if something in 56 needs to change.  thanks

--- db/update_057-pending.sql
+++ db/update_057-pending.sql
@@ -1,1 +1,7 @@
 -- update 57
+
+-- IMPORTANT: THIS UPDATE IS NOT YET FINISHED, DONT USE!
+
+-- Queries that are pending an update should be placed here. This allows them
+-- to be verified as working together. Once complete, the -pending suffix will
+-- be removed.
