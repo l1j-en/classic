539bc0ad43ac8f3af797eaf42a5f6f9c12660355
tricid <tricid@l1j.org>
2011-06-24 14:08:49 +0000

    finalized update 57

---
+++ db/update_058-pending.sql
@@ -0,0 +1,7 @@
+-- IMPORTANT: THIS UPDATE IS NOT YET FINISHED, DONT USE!
+
+-- Queries that are pending an update should be placed here. This allows them
+-- to be verified as working together. Once complete, the -pending suffix will
+-- be removed.
+
+-- update 58
