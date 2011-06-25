80ad1f47a6fe068d7cde6118c677cacc95702a7a
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-06-25 01:24:37 +0000

    Added the two non-interactive arachnes to Nerupa's cave.

--- db/update_058-pending.sql
+++ db/update_058-pending.sql
@@ -157,3 +157,8 @@ update shop set purchasing_price = 3 where item_id in (40006, 40007, 40008, 4000
 
 -- Correct dwarvish iron helm sell price.
 update shop set purchasing_price = 150 where item_id = 20007;
+
+-- Add non-interactive arachne for Nerupa's cave.
+insert into npc values (81246, 'Arachne', '$752', '', 'L1Guardian', 864, 30, 3000, 300, 0, 30, 30, 30, 30, 30, 70, 0, 0, 'small', 0, 2, 0, 420, 840, 0, 840, 840, 0, 0, 0, 1, 1, 1, '', 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0);
+-- 2 aranches in Nerupa's cave
+insert into spawnlist_npc values (1310419, 'Arachne', 2, 81246, 32739, 32800, 0, 0, 4, 0, 17, 0);
