19c3287b0650b27adcf5b300b169e3c5da7c1c59
l1valakas@gmail.com <l1valakas@gmail.com@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-06-26 15:33:44 +0000

    fixed an issue with loc of spawns and re-spawn delay (was way too long)

--- db/update_059-pending.sql
+++ db/update_059-pending.sql
@@ -5,3 +5,17 @@
 -- be removed.
 
 -- update 59
+
+-- Fixed Orc Emissary respawn delay and loc --
+update spawnlist set locx = '32721' where id = 400001;
+update spawnlist set locy = '32384' where id = 400001;
+update spawnlist set min_respawn_delay = '240' where id = 400001;
+update spawnlist set max_respawn_delay = '420' where id = 400001;
+update spawnlist set locx = '32674' where id = 400002;
+update spawnlist set locy = '32571' where id = 400002;
+update spawnlist set min_respawn_delay = '240' where id = 400002;
+update spawnlist set max_respawn_delay = '420' where id = 400002;
+update spawnlist set locx = '32631' where id = 400003;
+update spawnlist set locy = '33105' where id = 400003;
+update spawnlist set min_respawn_delay = '240' where id = 400002;
+update spawnlist set max_respawn_delay = '420' where id = 400002;
