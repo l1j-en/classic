f8a77935ba22d87e348c76447ec36d5bef21ce58
special_darkwolf@hotmail.com <special_darkwolf@hotmail.com@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-06-27 17:52:35 +0000

    walking npc htm fixes.

--- db/update_060-pending.sql
+++ db/update_060-pending.sql
@@ -10,3 +10,54 @@
 update skills set buffDuration = 1200 where skill_id = 79;
 -- De-aggro Minotaur (axe).
 update npc set agro = 0, agrososc = 0, agrocoi = 0 where npcid = 45390;
+
+
+-- walking npcaction fixes
+INSERT INTO npcaction VALUES ('70901', 'jeti1', '', '', '');
+INSERT INTO npcaction VALUES ('70604', 'bridget1', '', '', '');
+INSERT INTO npcaction VALUES ('70615', 'alda1', '', '', '');
+INSERT INTO npcaction VALUES ('70619', 'alice1', '', '', '');
+INSERT INTO npcaction VALUES ('70621', 'evelyn1', '', '', '');
+INSERT INTO npcaction VALUES ('70624', 'oliver1', '', '', '');
+INSERT INTO npcaction VALUES ('70626', 'jack1', '', '', '');
+INSERT INTO npcaction VALUES ('70634', 'tanya1', '', '', '');
+INSERT INTO npcaction VALUES ('70582', 'neil1', '', '', '');
+INSERT INTO npcaction VALUES ('70583', 'nelson1', '', '', '');
+INSERT INTO npcaction VALUES ('70606', 'victor1', '', '', '');
+INSERT INTO npcaction VALUES ('70608', 'shalina1', '', '', '');
+INSERT INTO npcaction VALUES ('70825', 'ellne1', '', '', '');
+INSERT INTO npcaction VALUES ('70839', 'doettCE1', '', '', '');
+INSERT INTO npcaction VALUES ('70854', 'hurindalien1', '', '', '');
+INSERT INTO npcaction VALUES ('70803', 'anotte1', '', '', '');
+INSERT INTO npcaction VALUES ('70858', 'derick1', '', '', '');
+INSERT INTO npcaction VALUES ('70581', 'gulian1', '', '', '');
+INSERT INTO npcaction VALUES ('70586', 'daisy1', '', '', '');
+INSERT INTO npcaction VALUES ('70592', 'leal1', '', '', '');
+INSERT INTO npcaction VALUES ('70593', 'manus1', '', '', '');
+INSERT INTO npcaction VALUES ('70589', 'laban1', '', '', '');
+INSERT INTO npcaction VALUES ('70591', 'lina1', '', '', '');
+INSERT INTO npcaction VALUES ('70637', 'tovia1', '', '', '');
+INSERT INTO npcaction VALUES ('70630', 'giles1', '', '', '');
+INSERT INTO npcaction VALUES ('70639', 'ferdinand1', '', '', '');
+INSERT INTO npcaction VALUES ('70576', 'garth1', '', '', '');
+INSERT INTO npcaction VALUES ('70640', 'pierre1', '', '', '');
+INSERT INTO npcaction VALUES ('70834', 'torban1', '', '', '');
+INSERT INTO npcaction VALUES ('70833', 'tommy1', '', '', '');
+INSERT INTO npcaction VALUES ('70828', 'ofo1', '', '', '');
+INSERT INTO npcaction VALUES ('70821', 'sanita1', '', '', '');
+INSERT INTO npcaction VALUES ('70814', 'ruka1', '', '', '');
+INSERT INTO npcaction VALUES ('70812', 'rohan1', '', '', '');
+INSERT INTO npcaction VALUES ('70819', 'bob1', '', '', '');
+INSERT INTO npcaction VALUES ('70767', 'haro1', '', '', '');
+INSERT INTO npcaction VALUES ('70735', 'garuga1', '', '', '');
+INSERT INTO npcaction VALUES ('70736', 'derian1', '', '', '');
+INSERT INTO npcaction VALUES ('70741', 'dima1', '', '', '');
+INSERT INTO npcaction VALUES ('70752', 'biyan1', '', '', '');
+INSERT INTO npcaction VALUES ('70746', 'rotus1', '', '', '');
+INSERT INTO npcaction VALUES ('70659', 'atara1', '', '', '');
+INSERT INTO npcaction VALUES ('70648', 'daley1', '', '', '');
+INSERT INTO npcaction VALUES ('70647', 'gavin1', '', '', '');
+INSERT INTO npcaction VALUES ('70867', 'shannon1', '', '', '');
+INSERT INTO npcaction VALUES ('70871', 'annabel1', '', '', '');
+INSERT INTO npcaction VALUES ('70877', 'trion1', '', '', '');
+INSERT INTO npcaction VALUES ('70878', 'felix1', '', '', '');
