8ff52cb7ce4bdc0ad2aac2c34923f00c9194da78
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-06-27 02:12:45 +0000

    Changed adv back to 1200 sec and minotaur (axe) back to aggro family only.

--- db/update_060-pending.sql
+++ db/update_060-pending.sql
@@ -5,3 +5,8 @@
 -- be removed.
 
 -- update 60
+
+-- Change AS back to 1200 sec.
+update skills set buffDuration = 1200 where skill_id = 79;
+-- De-aggro Minotaur (axe).
+update npc set agro = 0, agrososc = 0, agrocoi = 0 where npcid = 45390;
