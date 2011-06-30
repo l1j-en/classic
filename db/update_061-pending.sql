34c2d44cdd2f80b0549dee10712384bae6b51432
bm3719 <bm3719@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-06-30 01:07:09 +0000

    Finished etcitem merge.  Fixes several broken treasure box items, updates a ton of item descrs, adds new use_type, updates a few inv and ground graphics, etc.

--- db/update_061-pending.sql
+++ db/update_061-pending.sql
@@ -1196,7 +1196,20 @@ update etcitem set locx = 32782, locy = 32736 where item_id = 42003;
 update etcitem set locx = 32728, locy = 32815 where item_id = 42004;
 update etcitem set invgfx = 2878 where item_id = 42029;
 
-
+update etcitem set locx = 32572, locy = 32663 where item_id = 40083;
+-- Updated treasure XML to fix this.  JP has this confused with 40696.
+update etcitem set use_type = 'treasure_box' where item_id = 40697;
+update etcitem set invgfx = 2878 where item_id = 42030;
+update etcitem set invgfx = 2878 where item_id = 42031;
+update etcitem set invgfx = 2878 where item_id = 42032;
+update etcitem set invgfx = 2878 where item_id = 42033;
+update etcitem set invgfx = 2878 where item_id = 42035;
+update etcitem set invgfx = 2878 where item_id = 42036;
+update etcitem set invgfx = 2878 where item_id = 42037;
+update etcitem set invgfx = 2878 where item_id = 42038;
+update etcitem set invgfx = 2878 where item_id = 42039;
+update etcitem set stackable = 1 where item_id = 49032;
+update etcitem set stackable = 1 where item_id = 49033;
 
 
 
