9e54a7d54a3ff7135a5e3781936412052bdde128
special_darkwolf@hotmail.com <special_darkwolf@hotmail.com@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-07-02 22:42:47 +0000

    added teleport prices. i still need todo some teleports and prices. i will do this asap. i need to fix something else now.

--- src/l1j/server/server/model/Instance/L1TeleporterInstance.java
+++ src/l1j/server/server/model/Instance/L1TeleporterInstance.java
@@ -152,8 +152,29 @@ public class L1TeleporterInstance extends L1NpcInstance {
 				} else if (player.isWizard() || player.isDarkelf()) {
 					htmlid = "barnia1";
 				}
+		} else if (npcid == 81258) {
+			if (player.isIllusionist()) {
+				htmlid = "asha1";
+			} else {
+				htmlid = "asha2";
 			}
-
+		} else if (npcid == 81259) {
+			if (player.isDragonKnight()) {
+				htmlid = "feaena1";
+			} else {
+				htmlid = "feaena2";
+			}
+		} else if (npcid == 71013) {
+			if (player.isDarkelf()) {
+				if (player.getLevel() < 14) {
+					htmlid = "karen1";
+				} else {
+					htmlid = "karen4";
+				}
+			} else {
+				htmlid = "karen2";
+			    }
+			}
 			if (htmlid != null) {
 				player.sendPackets(new S_NPCTalkReturn(objid, htmlid));
 			} else {
@@ -175,16 +196,239 @@ public class L1TeleporterInstance extends L1NpcInstance {
 		int objid = getId();
 		L1NpcTalkData talking = NPCTalkDataTable.getInstance().getTemplate(
 				getNpcTemplate().get_npcId());
+		String[] price = null;
 		if (action.equalsIgnoreCase("teleportURL")) {
 			L1NpcHtml html = new L1NpcHtml(talking.getTeleportURL());
-			player.sendPackets(new S_NPCTalkReturn(objid, html));
+			int npcid = getNpcTemplate().get_npcId();
+			switch (npcid) {
+				case 50015: {
+					price = new String[] { "1500" };
+				}
+				break;
+				case 50020: {
+					price = new String[] { "55", "55", "55", "180", "180", "180", "180", "270", "270", "300", "300", "900", "10650" };
+				}
+				break;
+				case 50024: {
+					price = new String[] { "55", "55", "55", "55", "198", "55", "132", "264", "55", "198", "264", "220", "220", "550", "7480" };
+				}
+				break;
+				case 50036: {
+					price = new String[] { "55", "55", "55", "180", "180", "180", "180", "270", "270", "450", "450", "1050", "11100" };
+				}
+				break;
+				case 50039: {
+					price = new String[] { "55", "55", "180", "180", "270", "270", "270", "360", "360", "600", "600", "1200", "11550" };
+				}
+				break;
+				case 50044: {
+					price = new String[] { "50", "120", "120", "180", "180", "180", "240", "240", "300", "500", "500", "900", "7400" };
+				}
+				break;
+				case 50046: {
+					price = new String[] { "50", "120", "120", "120", "180", "180", "180", "240", "300", "500", "500", "900", "7400" };
+				}
+				break;
+				case 50051: {
+					price = new String[] { "55", "180", "270", "270", "360", "360", "360", "450", "450", "750", "750", "1350", "12000" };
+				}
+				break;
+				case 50054: {
+					price = new String[] { "55", "55", "55", "180", "180", "270", "270", "360", "450", "300", "300", "750", "9750" };
+				}
+				break;
+				case 50056: {
+					price = new String[] { "55", "55", "55", "132", "132", "132", "198", "198", "880", "880", "264", "330", "330", "880", "7480" };
+				}
+				break;
+				case 50066: {
+					price = new String[] { "55", "55", "55", "132", "132", "132", "198", "198", "264", "440", "440", "880", "7810" };
+				}
+				break;
+				case 50068: {
+					price = new String[] { "1500", "800", "600", "1800", "1800", "1000" };
+				}
+				break;
+				case 50026: {
+					price = new String[] { "550", "700", "810" };
+				}
+				break;
+				case 50033: {
+					price = new String[] { "560", "720", "560" };
+				}
+				break;
+				case 50049: {
+					price = new String[] { "1150", "980", "590" };
+				}
+				break;
+				case 50059: {
+					price = new String[] { "580", "680", "680" };
+				}
+				break;
+				case 50072: {
+					price = new String[] { "2200", "1800", "1000", "1600", "2200", "1200", "1300", "2000", "2000", "500", "500", "700", "650", "700", "800", "800", "1000" };
+				}
+				break;
+				case 50073: {
+					price = new String[] { "2200", "1800", "1000", "1600", "2200", "1200", "1300", "0", "2000", "2000" };
+				}
+				break;
+				case 50079: {
+					price = new String[] { "550", "550", "600", "550", "700", "600", "600", "750", "750", "500", "500", "700", "650", "700", "800", "800", "1000" };
+				}
+				break;
+				case 50080: {
+					price = new String[] { "15" };
+				}
+				break;
+				case 71039: {
+					price = new String[] { "10000" };
+				}
+				break;
+				case 71078: {
+					price = new String[] { "10000" };
+				}
+				break;
+				case 71080: {
+					price = new String[] { "10000" };
+				}
+				break;
+				case 80132: {
+					price = new String[] { "132", "55", "55", "55", "132", "132", "132", "132", "198", "198", "246", "330", "330", "770", "7480" };
+				}
+				break;
+				case 80146: {
+					price = new String[] { "132", "55", "55", "55", "132", "132", "132", "198", "198", "300", "246", "330", "330", "770", "7480" };
+				}
+				break;
+				default: {
+					price = new String[] { "" };
+				}
+			}
+			player.sendPackets(new S_NPCTalkReturn(objid, html, price));
+		} else if (action.equalsIgnoreCase("teleportURLA")) {
+			String html = "";
+			int npcid = getNpcTemplate().get_npcId();
+			switch (npcid) {
+				case 50020: {
+					html = "telekent3";
+					price = new String[] { "150", "330", "330", "330", "330", "330", "495", "495", "495", "660", "660" };
+				}
+				break;
+				case 50024: {
+					html = "telegludin3";
+					price = new String[] { "140", "308", "308", "308", "462", "462", "462", "462", "616", "770", "770" };
+				}
+				break;
+				case 50036: {
+					html = "telegiran3";
+					price = new String[] { "150", "150", "150", "330", "330", "330", "330", "495", "495", "495", "660" };
+				}
+				break;
+				case 50039: {
+					html = "televala3";
+					price = new String[] { "150", "330", "330", "330", "495", "495", "495", "495", "495", "660", "660" };
+				}
+				break;
+				case 50044: {
+					html = "sirius3";
+					price = new String[] { "100", "220", "220", "220", "330", "330", "440", "440", "550", "550", "550" };
+				}
+				break;
+				case 50051: {
+					html = "kirius3";
+					price = new String[] { "150", "330", "495", "495", "495", "660", "660", "825", "825", "825", "825" };
+				}
+				break;
+				case 50046: {
+					html = "elleris3";
+					price = new String[] { "100", "220", "220", "220", "330", "330", "440", "440", "550", "550", "550" };
+				}
+				break;
+				case 50054: {
+					html = "telewoods3";
+					price = new String[] { "150", "150", "330", "330", "495", "495", "495", "495", "660", "825", "825" };
+				}
+				break;
+				case 50056: {
+					html = "telesilver3";
+					price = new String[] { "150", "150", "330", "330", "330", "330", "495", "495", "495", "495", "495" };
+				}
+				break;
+				case 50066: {
+					html = "teleheine3";
+					price = new String[] { "110", "110", "242", "242", "242", "242", "363", "363", "484", "484", "605" };
+				}
+				break;
+				case 80132: {
+					html = "dekabia3";
+					price = new String[] { "100", "220", "220", "220", "330", "330", "330", "330", "440", "440" };
+				}
+				break;
+				case 80146: {
+					html = "sharial3";
+					price = new String[] { "220", "330", "330", "330", "440", "440", "550", "550", "550", "550" };
+				}
+				break;
+				default: {
+					price = new String[] { "" };
+				}
+			}
+			player.sendPackets(new S_NPCTalkReturn(objid, html, price));
 		} else if (action.equalsIgnoreCase("teleportURLA")) {
 			L1NpcHtml html = new L1NpcHtml(talking.getTeleportURLA());
 			player.sendPackets(new S_NPCTalkReturn(objid, html));
+		} else if (action.equalsIgnoreCase("teleportURLC")) {
+			String htmlid = "guide_1_2";
+			price = new String[] { "403", "403", "403", "403", "923", "923" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLB")) {
+			String htmlid = "guide_1_1";
+			price = new String[] { "390", "390", "390", "390" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLD")) {
+			String htmlid = "guide_1_3";
+			price = new String[] { "480", "480", "480", "480", "630", "1080", "630" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLF")) {
+			String htmlid = "guide_2_2";
+			price = new String[] { "533", "533", "793", "663" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLE")) {
+			String htmlid = "guide_2_1";
+			price = new String[] { "600", "600", "750", "750" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLG")) {
+			String htmlid = "guide_2_3";
+			price = new String[] { "630", "780", "630", "1080", "930" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLI")) {
+			String htmlid = "guide_3_2";
+			price = new String[] { "663", "663", "663", "663", "1313", "1053", "793" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLH")) {
+			String htmlid = "guide_3_1";
+			price = new String[] { "750", "750", "750", "1200", "1050" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLJ")) {
+			String htmlid = "guide_3_3";
+			price = new String[] { "780", "780", "780", "780", "780", "1230", "1080" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLK")) {
+			String htmlid = "guide_4";
+			price = new String[] { "676", "676", "676", "676", "676", "1066", "936" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLA")) {
+			String htmlid = "dekabia3";
+			price = new String[] { "100", "220", "220", "220", "330", "330", "330", "330", "440", "440" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
+		} else if (action.equalsIgnoreCase("teleportURLA")) {
+			String htmlid = "sharial3";
+			price = new String[] { "220", "330", "330", "330", "440", "440", "550", "550", "550", "550" };
+			player.sendPackets(new S_NPCTalkReturn(objid, htmlid, price));
 		}
 		if (action.startsWith("teleport ")) {
-			_log.finest((new StringBuilder()).append("Setting action to : ")
-					.append(action).toString());
+			_log.finest((new StringBuilder()).append("Setting action to : ").append(action).toString());
 			doFinalAction(player, action);
 		}
 	}
