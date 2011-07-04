1764ef11ae7bf8a8dd03d6532d3f2c7a6ce1b55f
tricid <tricid@l1j.org>
2011-07-04 06:41:29 +0000

    small tweak to spawn counts in lasta ele room

--- db/update_062-pending.sql
+++ db/update_062-pending.sql
@@ -8,3 +8,6 @@
 
 -- make chaos agro players
 UPDATE npc SET agro=1, agrososc = 1, agrocoi = 1 where npcid = 45625;
+
+-- minor adjustment on lasta ele room spawn counts
+UPDATE spawnlist SET count = 8 WHERE mapid = 464 AND count = 10;
