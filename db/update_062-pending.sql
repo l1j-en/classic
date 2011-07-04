76af0c54e1bfb0f056984154b512d3a098d5b666
tricid <tricid@l1j.org>
2011-07-04 07:37:48 +0000

    fix issue 189 - salamander in FoM

--- db/update_062-pending.sql
+++ db/update_062-pending.sql
@@ -11,3 +11,6 @@ UPDATE npc SET agro=1, agrososc = 1, agrocoi = 1 where npcid = 45625;
 
 -- minor adjustment on lasta ele room spawn counts
 UPDATE spawnlist SET count = 8 WHERE mapid = 464 AND count = 10;
+
+-- remove incorrect salamander spawn in forrest of mirror
+DELETE FROM spawnlist WHERE id = 9756;
