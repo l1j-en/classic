8bdbb5ca358c8f8d4de744d8350e2b8de1d83869
l1valakas@gmail.com <l1valakas@gmail.com@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-07-12 22:02:55 +0000

    Added KillGlance Ring and crafter

--- db/update_065-pending.sql
+++ db/update_065-pending.sql
@@ -5,3 +5,8 @@
 -- be removed.
 
 -- update 65
+-- added KillGlance ring and crafter
+insert into npc values (90016,'KillGlance','KillGlance','KillGlance','L1Merchant',2337,0,0,0,0,0,0,0,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0,-1,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,14,0,1,0);
+insert into npcaction values (90016,'killglance1','','','');
+insert into spawnlist_npc values (1900012,'KillGlance',1,90016,34073,33109,0,0,6,0,4,0);
+insert into armor values (30009,'KillGlance`s Ring','KillGlance`s Ring','KillGlance`s Ring','ring','gold',3000,668,21,0,0,-1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0);
