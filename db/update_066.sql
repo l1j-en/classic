580644328f473e9a43a1c5da24e4dac01160a37c
tricid <tricid@l1j.org>
2011-07-27 11:14:49 -0500

    finalize update 66, put new db changes in 67 please, do not edit 66 any more

--- db/update_066.sql
+++ db/update_066.sql
@@ -1,9 +1,3 @@
--- IMPORTANT: THIS UPDATE IS NOT YET FINISHED, DONT USE!
-
--- Queries that are pending an update should be placed here. This allows them
--- to be verified as working together. Once complete, the -pending suffix will
--- be removed.
-
 -- update 66
 
 -- merge the differences between us and jp on these mobs
