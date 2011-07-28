3bcd86b919531a3d5f1f9a5778d6ea446a1942d7
tricid <tricid@l1j.org>
2011-07-28 15:14:58 -0500

    looks like I left this table out somewhere along the line

--- db/update_067-pending.sql
+++ db/update_067-pending.sql
@@ -14,3 +14,39 @@ UPDATE npc SET agro = 1, agrososc = 1, agrocoi = 1, atkspeed = 960, ranged = 1 W
 
 -- add start siege command
 INSERT INTO `commands` VALUES ('siege', '200', 'L1WarStart');
+
+-- ----------------------------
+
+-- Table structure for checkwartime
+
+-- ----------------------------
+
+CREATE TABLE `checkwartime` (
+
+  `castle_id` int(12) NOT NULL auto_increment,
+
+  `name` varchar(45) default NULL,
+
+  `isActive` int(1) NOT NULL default '1',
+
+  PRIMARY KEY  (`castle_id`)
+
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+
+
+INSERT INTO `checkwartime` VALUES ('1', 'Kent Castle', '0');
+
+INSERT INTO `checkwartime` VALUES ('2', 'Orc Castle', '0');
+
+INSERT INTO `checkwartime` VALUES ('3', 'Windawood Castle', '0');
+
+INSERT INTO `checkwartime` VALUES ('4', 'Giran Castle', '0');
+
+INSERT INTO `checkwartime` VALUES ('5', 'Heine Castle', '0');
+
+INSERT INTO `checkwartime` VALUES ('6', 'Dwarf Castle', '0');
+
+INSERT INTO `checkwartime` VALUES ('7', 'Aden Castle', '0');
+
+INSERT INTO `checkwartime` VALUES ('8', 'Diad Fortress', '0');
