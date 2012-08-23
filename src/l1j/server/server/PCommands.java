9da40d58cdd2d1fadeab81fbcb6a8f36f4139829
everial <everial@gmail.com>
2012-08-23 17:47:27 -0400

    First pass update of PCommands to fix -buff and standardize messages.

--- src/l1j/server/server/PCommands.java
+++ src/l1j/server/server/PCommands.java
@@ -18,8 +18,6 @@
  */
 package l1j.server.server;
 
-import static l1j.server.server.model.skill.L1SkillId.EARTH_BIND;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Arrays;
@@ -37,13 +35,67 @@ import l1j.server.server.serverpackets.S_SystemMessage;
 import l1j.server.server.templates.L1Skill;
 import l1j.server.server.utils.SQLUtil;
 
+import static l1j.server.server.model.skill.L1SkillId.EARTH_BIND;
+import static l1j.server.server.model.skill.L1SkillId.LIGHT;
+import static l1j.server.server.model.skill.L1SkillId.SHIELD;
+import static l1j.server.server.model.skill.L1SkillId.HOLY_WEAPON;
+import static l1j.server.server.model.skill.L1SkillId.DECREASE_WEIGHT;
+import static l1j.server.server.model.skill.L1SkillId.PHYSICAL_ENCHANT_DEX;
+import static l1j.server.server.model.skill.L1SkillId.PHYSICAL_ENCHANT_STR;
+import static l1j.server.server.model.skill.L1SkillId.BLESS_WEAPON;
+import static l1j.server.server.model.skill.L1SkillId.HASTE;
+import static l1j.server.server.model.skill.L1SkillId.GREATER_HASTE;
+import static l1j.server.server.model.skill.L1SkillId.BERSERKERS;
+import static l1j.server.server.model.skill.L1SkillId.IMMUNE_TO_HARM;
+import static l1j.server.server.model.skill.L1SkillId.ABSOLUTE_BARRIER;
+import static l1j.server.server.model.skill.L1SkillId.ADVANCE_SPIRIT;
+import static l1j.server.server.model.skill.L1SkillId.STORM_SHOT;
+import static l1j.server.server.model.skill.L1SkillId.EARTH_SKIN;
+import static l1j.server.server.model.skill.L1SkillId.NATURES_TOUCH;
+
 public class PCommands {
 	private static Logger _log = Logger.getLogger(PCommands.class.getName());
-	boolean spawnTF = false;
 	private static PCommands _instance;
+	
+	private static int[] PowerBuffSkills = { LIGHT, DECREASE_WEIGHT, 
+		PHYSICAL_ENCHANT_DEX, PHYSICAL_ENCHANT_STR, BLESS_WEAPON, GREATER_HASTE,
+		BERSERKERS, IMMUNE_TO_HARM, ABSOLUTE_BARRIER, ADVANCE_SPIRIT,
+		STORM_SHOT, EARTH_SKIN, NATURES_TOUCH };
 
-	private PCommands() {
-	}
+	private static int[] BuffSkills = { LIGHT, SHIELD, HOLY_WEAPON,
+		DECREASE_WEIGHT, PHYSICAL_ENCHANT_DEX, PHYSICAL_ENCHANT_STR,
+		BLESS_WEAPON, HASTE };
+
+	// Starting experiment to see whether caching common messages has an
+	// effect.
+	private static final S_SystemMessage DropHelp =
+		new S_SystemMessage("-drop all|mine|party on|off");
+	private static final S_SystemMessage CommandsHelp =
+		new S_SystemMessage("-warp 1-7, -karma, -buff, -bug, -drop, -help");
+	private static final S_SystemMessage CommandsHelpNoBuff =
+		new S_SystemMessage("-warp 1-7, -karma, -bug, -drop, -help");
+	private static final S_SystemMessage NoBuff =
+		new S_SystemMessage("The -buff command is disabled.");
+	private static final S_SystemMessage BuffLevel =
+		new S_SystemMessage("You must be level 45 to use -buff.");
+	private static final S_SystemMessage NoWarpArea =
+		new S_SystemMessage("You cannot -warp in this area.");
+	private static final S_SystemMessage NoWarpState =
+		new S_SystemMessage("You cannot -warp in your current state.");
+	private static final S_SystemMessage NoWarp =
+		new S_SystemMessage("The -warp command is disabled.");
+	private static final S_SystemMessage NoPowerBuff =
+		new S_SystemMessage("The -pbuff command is disabled.");
+	private static final S_SystemMessage WarpLimit =
+		new S_SystemMessage("-warp 1-7 only.");
+	private static final S_SystemMessage WarpHelp =
+		new S_SystemMessage("-warp 1-Pandora, 2-SKT, 3-Giran, 4-Werldern, 5-Oren, 6-Orc Town, 7-Silent Cavern");
+	private static final S_SystemMessage BugHelp =
+		new S_SystemMessage("-bug bugReport");
+	private static final S_SystemMessage BugThanks =
+		new S_SystemMessage("Bug reported. Thank you for your help!");
+
+	private PCommands() { }
 
 	public static PCommands getInstance() {
 		if (_instance == null) {
@@ -52,171 +104,132 @@ public class PCommands {
 		return _instance;
 	}
 
-	public void handleCommands(L1PcInstance _player, String cmd2) {
-		
+	public void handleCommands(L1PcInstance player, String cmd2) {
 		try {
 			if (cmd2.equalsIgnoreCase("help")) {
-				showPHelp(_player);
+				showPHelp(player);
 			} else if (cmd2.startsWith("buff")) {
-				buff(_player);
+				buff(player);
 			} else if (cmd2.startsWith("warp")) {
-				warp(_player, cmd2);
+				warp(player, cmd2);
 			} else if (cmd2.startsWith("pbuff")){
-				powerBuff(_player);
+				powerBuff(player);
 			} else if (cmd2.startsWith("bug")){
-				reportBug(_player, cmd2);
+				reportBug(player, cmd2);
 			} else if(cmd2.startsWith("karma")){
-				checkKarma(_player);
+				checkKarma(player);
 			} else if (cmd2.startsWith("drop")) {
-				setDropOptions(_player, cmd2);
+				setDropOptions(player, cmd2);
 			}
-			_log.info(_player.getName() + " used " + cmd2);
+			_log.info(player.getName() + " used " + cmd2);
 		} catch (Exception e) {
 			_log.log(Level.SEVERE, e.getLocalizedMessage(), e);
 		}
 	}
 
-	public void showPHelp(L1PcInstance _player){
-		if (Config.PLAYER_BUFF == true && Config.PLAYER_COMMANDS == true)
-			_player.sendPackets(new S_SystemMessage(
-					"-warp 1-7, -karma, -buff, -bug, -drop, -help"));
-		else
-			_player.sendPackets(new S_SystemMessage(
-					"-warp 1-7, -karma, -bug, -drop, -help"));
+	public void showPHelp(L1PcInstance player) {
+		player.sendPackets(Config.PLAYER_BUFF && Config.PLAYER_COMMANDS
+				? CommandsHelp : CommandsHelpNoBuff);
 	}
 
-	public void buff(L1PcInstance _player){
-		int[] skillZ = {2, 3, 8, 14, 26, 42, 48, 34, 78};
-		int time = 0;
-		
-		if (Config.PLAYER_BUFF == true && Config.PLAYER_COMMANDS == true){
-			if (_player.getLevel() < 45) {
-				_player.sendPackets(new S_SystemMessage("You must be lvl 45 to use this command."));
-				return;
-			}
-			if(_player.getLevel() >= 45){
-				for (int a = 0; a <= 2; a++){
-					L1Skill skill = SkillTable.getInstance().findBySkillId(skillZ[a]);
-					if (skill.getTarget().equals("buff")) {
-					new L1SkillUse().handleCommands(_player, skillZ[a], _player.getId(), _player.getX(), _player.getY(), null, time, L1SkillUse.TYPE_SPELLSC);
-					}
-				}
-			}
-			else if (_player.getLevel() >= 50){
-				for (int a = 0; a <= 5; a++){
-					L1Skill skill = SkillTable.getInstance().findBySkillId(skillZ[a]);
-					if (skill.getTarget().equals("buff")) {
-					new L1SkillUse().handleCommands(_player, skillZ[a], _player.getId(), _player.getX(), _player.getY(), null, time, L1SkillUse.TYPE_SPELLSC);
-					}
-				}
-			}
-			else if (_player.getLevel() >= 55){
-				for (int a = 0; a <= 7; a++){
-					L1Skill skill = SkillTable.getInstance().findBySkillId(skillZ[a]);
-					if (skill.getTarget().equals("buff")) {
-					new L1SkillUse().handleCommands(_player, skillZ[a], _player.getId(), _player.getX(), _player.getY(), null, time, L1SkillUse.TYPE_SPELLSC);
-					}
-				}
-			}
-			else if (_player.getLevel() >= 60){
-				for (int a = 0; a <= 8; a++){
-					L1Skill skill = SkillTable.getInstance().findBySkillId(skillZ[a]);
-					if (skill.getTarget().equals("buff")) {
-					new L1SkillUse().handleCommands(_player, skillZ[a], _player.getId(), _player.getX(), _player.getY(), null, time, L1SkillUse.TYPE_SPELLSC);
-					}
-				}
-			}
-		} else {
-			_player.sendPackets(new S_SystemMessage("Buff command is disabled."));
-		}	
-	}
-
-	public void powerBuff(L1PcInstance _player){
-		int[] skillZ = {2, 14, 26, 42, 43, 48, 54, 55, 68, 78, 79, 149, 151, 155, 158};
-		int time = 0;
-		if (Config.POWER_BUFF == true && Config.PLAYER_COMMANDS == true){
-			for (int a = 0; a <= 14; a++){
-				L1Skill skill = SkillTable.getInstance().findBySkillId(skillZ[a]);
-				if (skill.getTarget().equals("buff")) {
-				new L1SkillUse().handleCommands(_player, skillZ[a], _player.getId(), _player.getX(), _player.getY(), null, time, L1SkillUse.TYPE_SPELLSC);
-				}
-			}
-		}else if (Config.PLAYER_COMMANDS == true && !Config.POWER_BUFF == true){
-			_player.sendPackets(new S_SystemMessage("Power Buff is currently disabled!"));
+	public void buff(L1PcInstance player){
+		if (!Config.PLAYER_BUFF || !Config.PLAYER_COMMANDS) {
+			player.sendPackets(NoBuff);
+			return;
 		}
+
+		int level = player.getLevel();
+		if (level < 45) {
+			player.sendPackets(BuffLevel);
+			return;
+		}
+		int max = 3;
+		if (level >= 60)
+			max = BuffSkills.length;
+		else if (level >= 55)
+			max = 5;
+		else if (level >= 50)
+			max = 4;
+
+		L1SkillUse skillUse = new L1SkillUse();
+		for (int i = 0; i < max; i++)
+			skillUse.handleCommands(player, BuffSkills[i], player.getId(),
+					player.getX(), player.getY(), null, 0, 
+					L1SkillUse.TYPE_SPELLSC);
 	}
 
-	public void warp(L1PcInstance _player, String cmd2) {
-		if(!Config.WARP) {
-			_player.sendPackets(new S_SystemMessage("The -warp command is not avaliable on this server."));
+	public void powerBuff(L1PcInstance player) {
+		if (Config.POWER_BUFF && Config.PLAYER_COMMANDS) {
+			L1SkillUse skillUse = new L1SkillUse();
+			for (int i = 0; i < PowerBuffSkills.length; i++)
+				skillUse.handleCommands(player, PowerBuffSkills[i],
+						player.getId(), player.getX(), player.getY(), null, 0,
+						L1SkillUse.TYPE_SPELLSC);
+		} else if (Config.PLAYER_COMMANDS && !Config.POWER_BUFF)
+			player.sendPackets(NoPowerBuff);
+	}
+
+	public void warp(L1PcInstance player, String cmd2) {
+		if (!Config.WARP) {
+			player.sendPackets(NoWarp);
 			return;
 		}
 		
-		if (!_player.getLocation().getMap().isEscapable())
-		{
-			_player.sendPackets(new S_SystemMessage("The -warp command is not avaliable in this area."));
+		if (!player.getLocation().getMap().isEscapable()) {
+			player.sendPackets(NoWarpArea);
 			return;
 		}
 		
-		if (_player.isPrivateShop() || _player.hasSkillEffect(EARTH_BIND) || 
-				_player.isParalyzed() || _player.isPinkName() || 
-				_player.isSleeped() || _player.isDead() || 
-				_player.getMapId() == 99) {
-			_player.sendPackets(
-				new S_SystemMessage("You cannot warp in your current state."));
+		if (player.isPrivateShop() || player.hasSkillEffect(EARTH_BIND) || 
+				player.isParalyzed() || player.isPinkName() || 
+				player.isSleeped() || player.isDead() || 
+				player.getMapId() == 99) {
+			player.sendPackets(NoWarpState);
 			return;
 		}	
 			
 		try {
 			int i = Integer.parseInt(cmd2.substring(5));
-			int delaytimer = 30; 
 			if (i >= 1 && i <= 7 ) {
-				delaytimer *= 100;
+				Thread.sleep(3000);
 				switch (i) {
 					case 1: // Pandora
-						Thread.sleep(delaytimer);
-						L1Teleport.teleport(_player, 32644, 32955, (short) 0, 5, true);
+						L1Teleport.teleport(player, 32644, 32955, (short) 0, 5, true);
 						break;
 					case 2: // SKT
-						Thread.sleep(delaytimer);
-						L1Teleport.teleport(_player, 33080, 33392, (short) 4, 5, true);
+						L1Teleport.teleport(player, 33080, 33392, (short) 4, 5, true);
 						break;
 					case 3: // Giran
-						Thread.sleep(delaytimer);
-						L1Teleport.teleport(_player, 33442, 32797, (short) 4, 5, true);
+						L1Teleport.teleport(player, 33442, 32797, (short) 4, 5, true);
 						break;
 					case 4: // Weldern
-						Thread.sleep(delaytimer);
-						L1Teleport.teleport(_player, 33705, 32504, (short) 4, 5, true);
+						L1Teleport.teleport(player, 33705, 32504, (short) 4, 5, true);
 						break;
 					case 5: // Oren
-						Thread.sleep(delaytimer);
-						L1Teleport.teleport(_player, 34061, 32276, (short) 4, 5, true);
+						L1Teleport.teleport(player, 34061, 32276, (short) 4, 5, true);
 						break;
 					case 6: // Orc Town
-						Thread.sleep(delaytimer);
-						L1Teleport.teleport(_player, 32715, 32448, (short) 4, 5, true);
+						L1Teleport.teleport(player, 32715, 32448, (short) 4, 5, true);
 						break;
 					case 7: // Silent Cave
-						Thread.sleep(delaytimer);
-						L1Teleport.teleport(_player, 32857, 32898, (short) 304, 5, true);
+						L1Teleport.teleport(player, 32857, 32898, (short) 304, 5, true);
 				}
 			} else {
-				_player.sendPackets(new S_SystemMessage("Warp 1-7 only."));
+				player.sendPackets(WarpLimit);
 			}
 		} catch (Exception exception) {
-			_player.sendPackets(new S_SystemMessage("-warp 1-Pandora, 2-SKT, 3-Giran, 4-Werldern, 5-Oren, 6-Orc Town, 7-Silent Cavern"));
+			player.sendPackets(WarpHelp);
 		}
 	}
 
-	private void reportBug(L1PcInstance pc, String bug){
+	private void reportBug(L1PcInstance pc, String bug) {
 		Connection con = null;
 		PreparedStatement pstm = null;
 		
 		try {
 			bug = bug.substring(3).trim();
 			if(bug.equals("")) {
-				pc.sendPackets(new S_SystemMessage(".bug bugReport"));
+				pc.sendPackets(BugHelp);
 				return;
 			}
 			con = L1DatabaseFactory.getInstance().getConnection();
@@ -227,9 +240,11 @@ public class PCommands {
 			pstm.setInt(4, pc.getX());
 			pstm.setInt(5, pc.getY());
 			pstm.execute();
-			pc.sendPackets(new S_SystemMessage("Bug reported. Thank you for your help!"));
+			pc.sendPackets(BugThanks);
 		} catch (Exception e) {
-			pc.sendPackets(new S_SystemMessage("Could not report bug: ('"+bug+"', '"+pc.getName()+"', "+pc.getMapId()+", "+pc.getX()+", "+pc.getY()+");"));
+			pc.sendPackets(new S_SystemMessage("Could not report bug: ('" + bug +
+						"', '" + pc.getName() + "', " + pc.getMapId() + ", " +
+						pc.getX() + ", " + pc.getY() + ");"));
 			e.printStackTrace();
 		} finally {
 			SQLUtil.close(pstm);
@@ -237,33 +252,23 @@ public class PCommands {
 		}
 	}
 
-	private void checkKarma(L1PcInstance pc){
-		pc.sendPackets(new S_SystemMessage("Your karma is currently: " + pc.getKarma() + "."));
-	
+	private void checkKarma(L1PcInstance pc) {
+		pc.sendPackets(new S_SystemMessage("Your karma is currently: " + 
+					pc.getKarma() + "."));
 	}
 	
 	private void setDropOptions(final L1PcInstance pc, final String options) {
 		List<String> pieces = Arrays.asList(options.split("\\s"));
+		boolean on = pieces.get(2).equals("on");
 		if (pieces.get(1).equals("all")) {
-			if (pieces.get(2).equals("on")) {
-				pc.setDropMessages(true);
-				pc.setPartyDropMessages(true);
-			} else {
-				pc.setDropMessages(false);
-				pc.setPartyDropMessages(false);
-			}
+			pc.setDropMessages(on);
+			pc.setPartyDropMessages(on);
 		} else if (pieces.get(1).equals("party")) {
-			if (pieces.get(2).equals("on"))
-				pc.setPartyDropMessages(true);
-			else
-				pc.setPartyDropMessages(false);
+			pc.setPartyDropMessages(on);
 		} else if (pieces.get(1).equals("mine")) {
-			if (pieces.get(2).equals("on"))
-				pc.setDropMessages(true);
-			else
-				pc.setDropMessages(false);
+			pc.setDropMessages(on);
 		} else {
-			pc.sendPackets(new S_SystemMessage("-drop all|mine|party on|off"));
+			pc.sendPackets(DropHelp);
 		}
 	}
 }
