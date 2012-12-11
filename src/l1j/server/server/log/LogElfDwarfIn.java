1d8f768a604dfe13711d8c51eb741830fcb30957
everial <everial@gmail.com>
2012-12-11 12:11:43 -0500

    Fix elf storage logging.

--- src/l1j/server/server/log/LogElfDwarfIn.java
+++ src/l1j/server/server/log/LogElfDwarfIn.java
@@ -33,6 +33,8 @@ import l1j.server.server.utils.SQLUtil;
 
 public class LogElfDwarfIn {
 	private static Logger _log = Logger.getLogger(LogElfDwarfIn.class.getName());
+	private static SimpleDateFormat FORMATTER =
+		new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	public void storeLogElfDwarfIn(L1PcInstance pc, L1ItemInstance item, int item_count_before,
 			int item_count_after, int item_in_count) {
@@ -40,11 +42,9 @@ public class LogElfDwarfIn {
 		PreparedStatement pstm = null;
 		try {
 			con = L1DatabaseFactory.getInstance().getConnection();
-			pstm = con
-					.prepareStatement("INSERT INTO LogElfWareHouseIn VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+			pstm = con.prepareStatement("INSERT INTO LogElfWareHouseIn VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
 			Date time = new Date();
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			String fm = formatter.format(time.getTime());
+			String fm = FORMATTER.format(time.getTime());
 			pstm.setString(1, fm);
 			pstm.setString(2, pc.getNetConnection().getIp());
 			pstm.setString(3, pc.getAccountName());
