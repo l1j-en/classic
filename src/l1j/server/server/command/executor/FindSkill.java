121cff89edd14bcb93e50c92520c77cbe3d8edbb
everial <everial@gmail.com>
2013-01-13 01:01:03 -0500

    Add .findskill to look up skill names or ids.

---
+++ src/l1j/server/server/command/executor/FindSkill.java
@@ -0,0 +1,64 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+package l1j.server.server.command.executor;
+
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_SystemMessage;
+import l1j.server.server.model.skill.L1NamedSkill;
+
+import java.util.List;
+
+public class FindSkill implements L1CommandExecutor {
+
+	private FindSkill() {}
+
+	public static L1CommandExecutor getInstance() {
+		return new FindSkill();
+	}
+
+	@Override
+	public void execute(L1PcInstance user, String commandName, String target) {
+		try {
+			findSkill(user, target);
+		} catch (Exception e) {
+			e.printStackTrace();
+			user.sendPackets(new S_SystemMessage(".findSkill <name|number>."));
+		}
+	}
+	
+	private void findSkill(L1PcInstance gm, String query) {
+		if (query.isEmpty()) {
+			gm.sendPackets(new S_SystemMessage(".findskill <name|number>."));
+			return;
+		}
+		try {
+			Integer id = Integer.valueOf(query);
+			gm.sendPackets(new S_SystemMessage(String.valueOf(id) + " is " + 
+					L1NamedSkill.getName(id) + "."));
+		} catch (NumberFormatException e) {
+			List<L1NamedSkill> skills = L1NamedSkill.searchNames(query);
+			String message = "Found: ";
+			for (L1NamedSkill skill : skills) {
+				message += " |" + " " + skill.name() + " " + skill.getId(); 
+			}
+			message += ".";
+			gm.sendPackets(new S_SystemMessage(message));
+		}
+	}
+}
