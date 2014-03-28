818d6b9d192afdd0823f4a5b39379b82757f8f47
soren <soren@l1j.org>
2014-03-28 11:27:41 -0400

    Fixes #39 where sometimes NPC for lvl 30 DK quest would spawn inside a wall.

--- db/update_077-pending.sql
+++ db/update_077-pending.sql
@@ -278,3 +278,6 @@ UPDATE `spawnlist` set `count` = 10 WHERE `mapid` = 2 and `npc_templateid` = 451
 -- Fix intervals for some morphs triggering speedhack detection
 UPDATE `spr_action` SET `framecount` = 17 WHERE `spr_id` = 6137;
 
+-- Fix Orc Emissary sometimes spawning inside wall in Heine 3F
+UPDATE `spawnlist_npc` SET `randomx` = 0, `randomy` = 0 WHERE `npc_templateid` = 81245 AND `mapid` = 61;
+
