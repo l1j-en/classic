0990c14cc544ae985048c5533953a872f7b93d7c
everial <everial@gmail.com>
2014-06-26 23:54:20 -0700

    Removed duplicate class constants and made L1ClassId the sole authority.

--- src/l1j/server/server/model/classes/L1ClassId.java
+++ src/l1j/server/server/model/classes/L1ClassId.java
@@ -18,6 +18,9 @@
  */
 package l1j.server.server.model.classes;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class L1ClassId {
 	public static final int ROYAL = 0;
 	public static final int KNIGHT = 1;
@@ -33,18 +36,115 @@ public class L1ClassId {
 	public static final int Del_DARK_ELF = 36;
 	public static final int Del_DRAGON_KNIGHT = 37;
 	public static final int Del_ILLUSIONIST = 38;
-	public static final int PRINCE = 0;
-	public static final int PRINCESS = 1;
-	public static final int ELF_MALE = 138;
-	public static final int ELF_FEMALE = 37;
-	public static final int KNIGHT_MALE = 61;
-	public static final int KNIGHT_FEMALE = 48;
-	public static final int WIZARD_MALE = 734;
-	public static final int WIZARD_FEMALE = 1186;
-	public static final int DARK_ELF_MALE = 2786;
-	public static final int DARK_ELF_FEMALE = 2796;
-	public static final int DRAGON_KNIGHT_MALE = 6658;
-	public static final int DRAGON_KNIGHT_FEMALE = 6661;
-	public static final int ILLUSIONIST_MALE = 6671;
-	public static final int ILLUSIONIST_FEMALE = 6650;
+	
+	// Really sprite ids.
+	public static final int CLASSID_PRINCE = 0;
+	public static final int CLASSID_PRINCESS = 1;
+	public static final int CLASSID_ELF_MALE = 138;
+	public static final int CLASSID_ELF_FEMALE = 37;
+	public static final int CLASSID_KNIGHT_MALE = 61;
+	public static final int CLASSID_KNIGHT_FEMALE = 48;
+	public static final int CLASSID_WIZARD_MALE = 734;
+	public static final int CLASSID_WIZARD_FEMALE = 1186;
+	public static final int CLASSID_DARK_ELF_MALE = 2786;
+	public static final int CLASSID_DARK_ELF_FEMALE = 2796;
+	public static final int CLASSID_DRAGON_KNIGHT_MALE = 6658;
+	public static final int CLASSID_DRAGON_KNIGHT_FEMALE = 6661;
+	public static final int CLASSID_ILLUSIONIST_MALE = 6671;
+	public static final int CLASSID_ILLUSIONIST_FEMALE = 6650;
+	
+	private static Logger _log = Logger.getLogger(L1ClassId.class.getName());
+	
+	public static String getSex(int classId) {
+		if (isMale(classId)) {
+			return "Male";
+		}
+		if (isFemale(classId)) {
+			return "Female";
+		}
+		_log.log(Level.SEVERE, 
+				"Unrecognized classId in #L1ClassId.getSex(). Check db.");
+		throw new IllegalStateException("Unrecognized character class id.");
+	}
+
+	private static boolean isMale(int classId) {
+		return classId == CLASSID_PRINCE
+				|| classId == CLASSID_KNIGHT_MALE
+				|| classId == CLASSID_ELF_MALE
+				|| classId == CLASSID_WIZARD_MALE
+				|| classId == CLASSID_DARK_ELF_MALE
+				|| classId == CLASSID_DRAGON_KNIGHT_MALE
+				|| classId == CLASSID_ILLUSIONIST_MALE;
+	}
+
+	private static boolean isFemale(int classId) {
+		return classId == CLASSID_PRINCESS
+				|| classId == CLASSID_KNIGHT_FEMALE
+				|| classId == CLASSID_ELF_FEMALE
+				|| classId == CLASSID_WIZARD_FEMALE
+				|| classId == CLASSID_DARK_ELF_FEMALE
+				|| classId == CLASSID_DRAGON_KNIGHT_FEMALE
+				|| classId == CLASSID_ILLUSIONIST_FEMALE;
+	}
+
+	public static String getClass(int classId) {
+		if (isRoyal(classId)) {
+			return "Royal";
+		}
+		if (isKnight(classId)) {
+			return "Knight";
+		}
+		if (isElf(classId)) {
+			return "Elf";
+		}
+		if (isMage(classId)) {
+			return "Mage";
+		}
+		if (isDarkElf(classId)) {
+			return "Dark Elf";
+		}
+		if (isDragonKnight(classId)) {
+			return "Dragon Knight";
+		}
+		if (isIllusionist(classId)) {
+			return "Illusionist";
+		}
+		_log.log(Level.SEVERE, 
+				"Unrecognized classId in #L1ClassId.getClass(). Check db.");
+		throw new IllegalStateException("Unrecognized character class id.");
+	}
+
+	public static boolean isRoyal(int classId) {
+		return classId == CLASSID_PRINCE || classId == CLASSID_PRINCESS;
+	}
+
+	public static boolean isKnight(int classId) {
+		return classId == CLASSID_KNIGHT_MALE
+				|| classId == CLASSID_KNIGHT_FEMALE;
+	}
+
+	public static boolean isElf(int classId) {
+		return classId == CLASSID_ELF_MALE 
+				|| classId == CLASSID_ELF_FEMALE;
+	}
+
+	public static boolean isMage(int classId) {
+		return classId == CLASSID_WIZARD_MALE 
+				|| classId == CLASSID_WIZARD_FEMALE;
+	}
+
+	public static boolean isDarkElf(int classId) {
+		return classId == CLASSID_DARK_ELF_MALE
+				|| classId == CLASSID_DARK_ELF_FEMALE;
+	}
+
+	public static boolean isDragonKnight(int classId) {
+		return classId == CLASSID_DRAGON_KNIGHT_MALE
+				|| classId == CLASSID_DRAGON_KNIGHT_FEMALE;
+	}
+
+	public static boolean isIllusionist(int classId) {
+		return classId == CLASSID_ILLUSIONIST_MALE
+				|| classId == CLASSID_ILLUSIONIST_FEMALE;
+	}
 }
