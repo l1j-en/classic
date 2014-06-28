fad6c9298796068866cc216244d205b697fc750c
Bruce C. Miller <bm3719@gmail.com>
2014-06-28 18:50:52 -0400

    Rebalance DI boss elixir droprates.

--- db/update_078-pending.sql
+++ db/update_078-pending.sql
@@ -3,4 +3,8 @@
 -- Queries that are pending an update should be placed here. This allows them
 -- to be verified as working together. Once complete, the -pending suffix will
 -- be removed.
+
 update armor set name = 'Boots of Dread King' where item_id = 20200;
+
+-- Rebalance DI boss elixir droprates.
+update droplist set chance = 200 where mobid in (45640, 45642, 45643, 45644, 45645) and itemid in (40033, 40034, 40035, 40036, 40037, 40038);
