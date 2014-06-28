5853387097f367e3bae82d626461aa6046de84ff
soren <soren@l1j.org>
2014-06-28 14:31:06 -0400

    Corrected DB name for Boots of Dread King

--- db/update_078-pending.sql
+++ db/update_078-pending.sql
@@ -3,3 +3,4 @@
 -- Queries that are pending an update should be placed here. This allows them
 -- to be verified as working together. Once complete, the -pending suffix will
 -- be removed.
+update armor set name = 'Boots of Dread King' where item_id = 20200;
