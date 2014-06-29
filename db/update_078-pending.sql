6dde0193dcf6167505305f5d8fcd069d9c9e12e8
Bruce C. Miller <bm3719@gmail.com>
2014-06-28 21:10:51 -0400

    Removed all freely acquired elven mats from shops. Fixes #120.

--- db/update_078-pending.sql
+++ db/update_078-pending.sql
@@ -8,3 +8,6 @@ update armor set name = 'Boots of Dread King' where item_id = 20200;
 
 -- Rebalance DI boss elixir droprates.
 update droplist set chance = 200 where mobid in (45640, 45642, 45643, 45644, 45645) and itemid in (40033, 40034, 40035, 40036, 40037, 40038);
+
+-- Remove all freely acquired elven mats from shops.
+delete from shop where item_id in (40519, 40502, 40503, 40504, 40505, 40506, 40507, 40515, 40520, 40521);
