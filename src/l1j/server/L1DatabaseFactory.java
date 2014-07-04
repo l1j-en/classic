73a4d09522400f3800c6179fdfe9c5fde531f863
tricid <tricid@l1j.org>
2014-07-04 18:27:37 -0500

    Add another bonecp config option

--- src/l1j/server/L1DatabaseFactory.java
+++ src/l1j/server/L1DatabaseFactory.java
@@ -55,8 +55,9 @@ public class L1DatabaseFactory {
 			_source.setPassword(_password);
 			_source.setPartitionCount(3);
 			_source.setCloseConnectionWatch(true);
-			_source.setLogStatementsEnabled(true);
+			//_source.setLogStatementsEnabled(true);
 			_source.setIdleConnectionTestPeriodInSeconds(10);
+			_source.setTransactionRecoveryEnabled(true);
 			/* Test the connection */
 			//_source.getConnection().close();
 		} catch (Exception e) {
