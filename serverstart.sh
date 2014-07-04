cbb5e13d957c4be5e3830f7b0ca3d4f5a51a3034
tricid <tricid@l1j.org>
2014-07-04 14:09:47 -0500

    I hate dependencies but I like bonecp enough to tolerate them. If this lib makes it to master I might just switch to slf4j universally, since it was forced on me for bonecp anyway.

--- serverstart.sh
+++ serverstart.sh
@@ -16,7 +16,7 @@ while :; do
   [ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
   [ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log" 
   java -Xms1024m -Xmx1024m -cp \
-      l1jen.jar:lib/c3p0-0.9.1.2.jar:lib/mysql-connector-java-5.1.31-bin.jar:lib/javolution.jar \
+      l1jen.jar:lib/slf4j-api-1.7.5.jar:lib/slf4j-simple-1.7.5.jar:.jar:lib/guava-17.0.jar:lib/bonecp-0.8.0.RELEASE.jar:lib/mysql-connector-java-5.1.31-bin.jar:lib/javolution.jar \
       l1j.server.Server | tee log/stdout.log 2>&1
  [ $? -ne 2 ] && break 
 #       /etc/init.d/mysql restart 
