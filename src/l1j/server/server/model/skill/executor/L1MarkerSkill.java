1040e250ca73c9740b338d7cfdae7cb80b3145c9
Bruce C. Miller <bm3719@gmail.com>
2014-07-29 18:54:02 -0400

    Removed all remaining Japanese text from code.

--- src/l1j/server/server/model/skill/executor/L1MarkerSkill.java
+++ src/l1j/server/server/model/skill/executor/L1MarkerSkill.java
@@ -2,10 +2,6 @@ package l1j.server.server.model.skill.executor;
 
 import l1j.server.server.model.L1Character;
 
-/**
- * SkillTimerに登録するだけで効力を持ち、追加や削除に特別な処理の必要ないスキル
- * 
- */
 public class L1MarkerSkill extends L1BuffSkillExecutorImpl {
 
 	@Override
