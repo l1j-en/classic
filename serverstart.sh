048c6415875b8d17591384db71c059e2e04af913
chriswall8 <chris.wall8@gmail.com>
2014-08-06 08:15:44 -0400

    Fix log rotation in serverstart.sh to match new log.properties options, and make serverstart.sh more friendly to being run as a daemon

--- serverstart.sh
+++ serverstart.sh
@@ -13,9 +13,9 @@ if [[ ! -e "emblem/" ]]; then
 fi
  
 while :; do 
-  [ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
+  [ -f log/java0.log ] && mv log/java0.log "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
   [ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log" 
-  java -Xms1024m -Xmx1024m -cp \
+  exec java -Xms1024m -Xmx1024m -cp \
       l1jen.jar:lib/slf4j-api-1.7.5.jar:lib/slf4j-simple-1.7.5.jar:.jar:lib/guava-17.0.jar:lib/bonecp-0.8.0.RELEASE.jar:lib/mysql-connector-java-5.1.31-bin.jar:lib/javolution.jar \
       l1j.server.Server | tee log/stdout.log 2>&1
  [ $? -ne 2 ] && break 
