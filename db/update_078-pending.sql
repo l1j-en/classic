e3e48cb8e84f5f46be28fb677a3d6d58d34c86d8
Chris Wall <chris.wall8@gmail.com>
2014-09-13 04:45:49 -0400

    Addendum to last commit for #72

--- db/update_078-pending.sql
+++ db/update_078-pending.sql
@@ -810,3 +810,5 @@ insert into spawnlist values ( null, 'Otyu', 12, 91207, 0, 32825, 32842, 60, 60,
 -- Add Heine Castle door to Sanctuary of Eva entrance
 INSERT INTO npc VALUES (71273, 'Gatekeeper', '$474', '', 'L1Guard', 148, 10, 3000, 300, 0, 126, 126, 126, 126, 126, 0, 0, 1800, 'large', 0, 2, 0, 480, 840, 0, 840, 840, 0, 0, 0, 0, 0, 0, 'guard', 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 14, 0, 0, 0);
 INSERT INTO spawnlist_door VALUES ('Heine Castle', 1336, 32734, 32790, 62, 0, 71273);
+UPDATE dungeon SET src_x = 32734, src_y = 32790 WHERE src_x = 32733 and src_y = 32791 and src_mapid = 62;
+INSERT INTO spawnlist_npc VALUES (88229, 'Gatekeeper', 1, 71273, 32734, 32793, 0, 0, 4, 0, 62, 100);
