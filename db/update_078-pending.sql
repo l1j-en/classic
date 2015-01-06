3965ba80e9eb7b408f0b4d0ed4eb3cbeaa4b067d
Bruce C. Miller <bm3719@gmail.com>
2015-01-06 09:27:30 -0500

    Corrected Robe of Dread Queen name.  Fixes #257.

--- db/update_078-pending.sql
+++ db/update_078-pending.sql
@@ -1007,3 +1007,7 @@ delete from shop where item_id in (40615, 40616, 40782, 40783);
 
 -- Set Secret Room Key to stackable
 update etcitem set stackable = 1 where item_id = 40555;
+
+-- Fix name of Robe of Dread Queen (was Robe of Evil Spirit King).
+update armor set name = 'Robe of Dread Queen' where item_id = 20109;
+update droplist set item_name = 'Robe of Dread Queen' where itemid = 20109;
