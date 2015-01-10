1c347a76d5039ed014199d259af0f2572ebb5627
Chris Wall <chris.wall8@gmail.com>
2015-01-09 22:41:11 -0500

    Finalize update_078.sql and add update_079-pending.sql

--- db/update_078.sql
+++ db/update_078.sql
@@ -1,9 +1,3 @@
--- IMPORTANT: THIS UPDATE IS NOT YET FINISHED, DONT USE!
-
--- Queries that are pending an update should be placed here. This allows them
--- to be verified as working together. Once complete, the -pending suffix will
--- be removed.
-
 update armor set name = 'Boots of Dread King' where item_id = 20200;
 
 -- Rebalance DI boss elixir droprates.
