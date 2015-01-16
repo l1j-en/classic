4eace5e7440acb4964a72336db6ce619cf8042b5
Chris Wall <chris.wall8@gmail.com>
2015-01-15 23:27:22 -0500

    Stop logging stdout (no longer used outside of Loggers), but keep logging stderr for uncaught exceptions

--- serverstart.sh
+++ serverstart.sh
@@ -14,10 +14,10 @@ fi
  
 while :; do 
   [ -f log/java0.log ] && mv log/java0.log "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
-  [ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log" 
-  exec java -Xms1024m -Xmx1024m -cp \
+  [ -f log/stderr.log ] && mv log/stderr.log "log/`date +%Y-%m-%d_%H-%M-%S`_stderr.log" 
+  (exec java -Xms1024m -Xmx1024m -cp \
       l1jen.jar:lib/slf4j-api-1.7.5.jar:lib/slf4j-simple-1.7.5.jar:.jar:lib/guava-17.0.jar:lib/bonecp-0.8.0.RELEASE.jar:lib/mysql-connector-java-5.1.31-bin.jar:lib/javolution.jar \
-      l1j.server.Server | tee log/stdout.log 2>&1
+      l1j.server.Server) 3>&1 1>&2 2>&3 | tee log/stderr.log
  [ $? -ne 2 ] && break 
 #       /etc/init.d/mysql restart 
          sleep 10 
