2102fc797c90e3e85a25fe0915b77425d5211cc9
L1J Smitty <l1jsmitty@gmail.com>
2016-02-13 10:22:25 -0500

    Fixes private shop no-aggro glitch

--- src/l1j/server/server/clientpackets/C_Shop.java
+++ src/l1j/server/server/clientpackets/C_Shop.java
@@ -61,8 +61,14 @@ public class C_Shop extends ClientBasePacket {
 			pc.sendPackets(new S_Disconnect());
 			return;
 		}
+		
 		int mapId = pc.getMapId();
 		if (mapId != 340 && mapId != 350 && mapId != 360 && mapId != 370) {
+			// if somehow they got out of the zone, 
+			// disable private shop
+			if(pc.isPrivateShop())
+				pc.setPrivateShop(false); 
+			
 			pc.sendPackets(new S_ServerMessage(876));
 			return;
 		}
