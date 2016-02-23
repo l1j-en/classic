b74c29d184ba2638b0a5f5a4268364a385f99be5
L1J Smitty <l1jsmitty@gmail.com>
2016-02-22 20:25:38 -0500

    Updated comment

--- src/l1j/server/server/serverpackets/S_ChatPacket.java
+++ src/l1j/server/server/serverpackets/S_ChatPacket.java
@@ -74,7 +74,7 @@ public class S_ChatPacket extends ServerBasePacket {
 			writeC(opcode);
 			writeC(type);
 			writeS("[" + pc.getName() + "] " + chat);
-		} else if (type == 13) { // Party chat
+		} else if (type == 13) { // alliance chat
 			writeC(opcode);
 			writeC(0x04);
 			writeS("{{" + pc.getName() + "}} " + chat);
