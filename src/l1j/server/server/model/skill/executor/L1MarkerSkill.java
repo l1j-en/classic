daa43daad52ab1d0f293d914479feb355d277f4d
L1J Smitty <l1jsmitty@gmail.com>
2016-02-22 22:32:46 -0500

    Remove erase magic icon/effects client side (was already server-side)

--- src/l1j/server/server/model/skill/executor/L1MarkerSkill.java
+++ src/l1j/server/server/model/skill/executor/L1MarkerSkill.java
@@ -1,6 +1,9 @@
 package l1j.server.server.model.skill.executor;
 
 import l1j.server.server.model.L1Character;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.model.skill.L1SkillId;
+import l1j.server.server.serverpackets.S_SkillIconAura;
 
 public class L1MarkerSkill extends L1BuffSkillExecutorImpl {
 
@@ -11,5 +14,9 @@ public class L1MarkerSkill extends L1BuffSkillExecutorImpl {
 
 	@Override
 	public void removeEffect(L1Character target) {
+		// TODO: you might be able to do if impl = 'L1MarkerSkill' and use the getSkillId() - 1
+		// but will leave that for future testing
+		if(_skill.getSkillId() == L1SkillId.ERASE_MAGIC && target instanceof L1PcInstance)
+			((L1PcInstance)target).sendPackets(new S_SkillIconAura(152, 0));
 	}
 }
