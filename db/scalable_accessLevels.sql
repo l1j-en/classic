1dde7fc6315f72d133a37771e4449eaea5cff15b
Chris Wall <chris.wall8@gmail.com>
2016-02-25 18:17:00 +0000

    Make access levels default to an id of 0 (player)

--- db/scalable_accessLevels.sql
+++ db/scalable_accessLevels.sql
@@ -6,6 +6,7 @@ UPDATE commands SET run_on_login = '1' WHERE name = 'invis';
 
 -- modify the access levels to the new system
 CREATE TABLE `access_levels` (id SMALLINT PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, chat_prefix VARCHAR(3), access_level SMALLINT NOT NULL default 0);
+INSERT INTO `access_levels` VALUES (0, 'Player', NULL, 0);
 INSERT INTO `access_levels` VALUES ('1', 'GameMaster', 'GM', 200);
 INSERT INTO `access_levels` VALUES ('2', 'Monitor', 'MON', 100);
 INSERT INTO `access_levels` (`id`, `name`, `access_level`) VALUES ('3', 'Player', 0);
@@ -17,8 +18,8 @@ UPDATE `accounts` set access_level = '1' WHERE access_level = '200';
 UPDATE `accounts` set access_level = '2' WHERE access_level = '100';
 
 -- Update these to short because that is what we use everywhere in code
-ALTER TABLE `accounts` MODIFY access_level SMALLINT;
-ALTER TABLE `characters` MODIFY accesslevel SMALLINT;
+ALTER TABLE `accounts` MODIFY access_level SMALLINT DEFAULT 0;
+ALTER TABLE `characters` MODIFY accesslevel SMALLINT DEFAULT 0;
 ALTER TABLE `characters` ADD CONSTRAINT fk_accesslevel FOREIGN KEY (accesslevel) REFERENCES access_levels(id);
 ALTER TABLE `accounts` ADD CONSTRAINT fk_access_level FOREIGN KEY (access_level) REFERENCES access_levels(id);
 
