5b093d4fb8a604d24924ef59d78f305078f5686b
L1J Smitty <l1jsmitty@gmail.com>
2016-06-26 20:23:36 -0400

    Fixed typo

--- db/update_gmcommands.sql
+++ db/update_gmcommands.sql
@@ -74,5 +74,5 @@ REPLACE INTO commands VALUES('trace', '100', 'L1Trace', 'Displays all accounts t
 REPLACE INTO commands VALUES('invul', '100', 'L1Invul', 'Makes you invulnerable to skills and attacks.');
 REPLACE INTO commands VALUES('movechar', '100', 'L1MoveChar', 'Allows you to move a character (online or offline) to a specific location.', 0);
 UPDATE commands SET access_level = '100' WHERE name = 'resolve';
-UPDATE commands SET `help_text` = 'Resolve a specific bug. Use .resolve -n # to nofity the reporter.' WHERE name = 'resolve';
+UPDATE commands SET `help_text` = 'Resolve a specific bug. Use .resolve -n # to notify the reporter.' WHERE name = 'resolve';
 INSERT INTO `commands` VALUES('spoof', 100, 'L1SpoofName', 0, 'Allows you to appear as a different name.');
