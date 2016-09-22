17022d4bd64c8d545169f1b024157dda21f34376
Zebra <zebrafrompluto@gmail.com>
2016-09-21 22:40:32 -0400

    Renamed setReloadTime() to resetReloadTime() as it makes more sense

--- src/l1j/server/server/command/executor/L1Reload.java
+++ src/l1j/server/server/command/executor/L1Reload.java
@@ -49,7 +49,7 @@ public class L1Reload implements L1CommandExecutor {
 				L1Teleport.teleport(pc, pc.getX(), pc.getY(), pc.getMapId(), 5,
 						false);
 				pc.sendPackets(new S_SystemMessage("Your screen has been refreshed"));
-				pc.setReloadTime();
+				pc.resetReloadTime();
 			} catch (Exception e) {
 				_log.log(Level.SEVERE, e.getLocalizedMessage(), e);
 			}
