2930b1f174dfb90ec40cc2e2e3e1bfc01d3ce2ed
L1J Smitty <l1jsmitty@gmail.com>
2017-03-13 22:44:25 -0400

    Added reason and timestamp to ban_ip

--- src/l1j/server/server/command/executor/L1BanIp.java
+++ src/l1j/server/server/command/executor/L1BanIp.java
@@ -38,50 +38,50 @@ public class L1BanIp implements L1CommandExecutor {
 	public void execute(L1PcInstance pc, String cmdName, String arg) {
 		try {
 			StringTokenizer stringtokenizer = new StringTokenizer(arg);
-			String s1 = stringtokenizer.nextToken();
-			String s2 = null;
-			try {
-				s2 = stringtokenizer.nextToken();
-			} catch (Exception e) {
-			}
-
+			String ip = stringtokenizer.nextToken();
+			String reason = "";
 			IpTable iptable = IpTable.getInstance();
-			boolean isBanned = iptable.isBannedIp(s1);
-
+			boolean isBanned = iptable.isBannedIp(ip);
+			
 			for (L1PcInstance tg : L1World.getInstance().getAllPlayers()) {
-				if (s1.equals(tg.getNetConnection().getIp())) {
-					String msg = new StringBuilder().append(" IP: ").append(s1)
+				if (ip.equals(tg.getNetConnection().getIp())) {
+					String msg = new StringBuilder().append(" IP: ").append(ip)
 							.append(" Player: ").append(tg.getName())
 							.toString();
 					pc.sendPackets(new S_SystemMessage(msg));
 				}
 			}
+			
+			String action = stringtokenizer.nextToken().toLowerCase();			
+			
+			while(stringtokenizer.hasMoreTokens())
+				reason += stringtokenizer.nextToken() + " ";
+			
+			reason = reason.trim();
 
-			if ("add".equals(s2) && !isBanned) {
-				iptable.banIp(s1);
-				String msg = new StringBuilder().append(" IP: ").append(s1)
-						.append(" has been banned.").toString();
+			if ("add".equals(action)) {
+				String msg = String.format("IP: %s is already banned.", ip);
+				
+				if(!isBanned) {
+					iptable.banIp(ip, reason);
+					msg = String.format("IP: %s has been banned.", ip);
+				}
+				
 				pc.sendPackets(new S_SystemMessage(msg));
-			} else if ("del".equals(s2) && isBanned) {
-				if (iptable.liftBanIp(s1)) {
-					String msg = new StringBuilder().append(" IP: ").append(s1)
-							.append(" has been unbanned.").toString();
-					pc.sendPackets(new S_SystemMessage(msg));
+			} else if ("del".equals(action)) {
+				String msg = String.format("IP: %s is not currently banned.", ip);
+				if(isBanned) {
+					if (iptable.liftBanIp(ip)) {
+						msg = String.format("IP: %s has been unbanned.", ip);
+					} else {
+						msg = String.format("Failed to ban IP: %s.", ip);
+					}
 				}
-			} else {
-
-				if (isBanned) {
-					String msg = new StringBuilder().append(" IP: ").append(s1)
-							.append(" BAN ").toString();
-					pc.sendPackets(new S_SystemMessage(msg));
-				} else {
-					String msg = new StringBuilder().append(" IP: ").append(s1)
-							.append(" BAN IP ").toString();
-					pc.sendPackets(new S_SystemMessage(msg));
-				}
+				
+				pc.sendPackets(new S_SystemMessage(msg));
 			}
 		} catch (Exception e) {
-			pc.sendPackets(new S_SystemMessage(cmdName + " IP [ add | del ]"));
+			pc.sendPackets(new S_SystemMessage(cmdName + " IP [ add | del ] [reason]"));
 		}
 	}
 }
