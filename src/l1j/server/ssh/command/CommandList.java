83d988a9183e3335a5de743b6d55fae3118c6d94
L1J Smitty <l1jsmitty@gmail.com>
2017-07-01 22:22:57 -0400

    SSH Server working and added CreateAccount and ChangePassword commands

---
+++ src/l1j/server/ssh/command/CommandList.java
@@ -0,0 +1,22 @@
+package l1j.server.ssh.command;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandList {
+	private static Map<String, Commands> _cmds = new HashMap<String, Commands>();
+	
+	static {
+		_cmds.put("echo", new Echo());
+		_cmds.put("playerid", new PlayerId());
+		_cmds.put("charstatus", new CharStatus());
+		_cmds.put("globalchat", new GlobalChat());
+		_cmds.put("shutdown", new ShutDown());
+		_cmds.put("createaccount", new CreateAccount());
+		_cmds.put("changepassword", new ChangePassword());
+	}
+	
+	public static Commands get(String name) {
+		return _cmds.get(name.toLowerCase());
+	}
+}
