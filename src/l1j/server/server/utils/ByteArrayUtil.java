0a4a28fbc8b8fa9fc125f3b7094d7f47962c3413
L1J Smitty <l1jsmitty@gmail.com>
2017-08-14 21:28:49 -0400

    Added -report player command and packet logging

--- src/l1j/server/server/utils/ByteArrayUtil.java
+++ src/l1j/server/server/utils/ByteArrayUtil.java
@@ -80,6 +80,16 @@ public class ByteArrayUtil {
 		}
 		return stringbuffer.toString();
 	}
+	
+	public static int[] convertToInt(byte[] input)
+	{
+	    int[] ret = new int[input.length];
+	    for (int i = 0; i < input.length; i++)
+	    {
+	        ret[i] = input[i] & 0xff; // Range 0 to 255, not -128 to 127
+	    }
+	    return ret;
+	}
 
 	private String fillHex(int i, int j) {
 		String s = Integer.toHexString(i);
