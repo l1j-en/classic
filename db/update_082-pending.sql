e3c2ee8b3688c42d5479f745acf47a3c4f8676cc
L1J Smitty <l1jsmitty@gmail.com>
2018-06-05 22:07:33 -0400

    fix walking onto FI boat from heine

--- db/update_082-pending.sql
+++ db/update_082-pending.sql
@@ -128,3 +128,5 @@ INSERT INTO `droplist` VALUES (91424,'Starved Warden',40308,'adena',80,200,60000
 -- fix Great Spirit of Wind spawns on DI to match other elements (3 of each on CASPA timer)
 INSERT INTO `spawnlist_boss` (`id`, `location`, `cycle_type`, `count`, `npc_id`, `group_id`, `locx`, `locy`, `randomx`, `randomy`, `locx1`, `locy1`, `locx2`, `locy2`, `heading`, `mapid`, `respawn_screen`, `movement_distance`, `rest`, `spawn_type`, `percentage`) VALUES (NULL, 'Great Spirit of Wind', 'Caspa', '1', '45644', '0', '32624', '32807', '0', '0', '32588', '32739', '32660', '32876', '4', '303', '1', '0', '0', '0', '100');
 INSERT INTO `spawnlist_boss` (`id`, `location`, `cycle_type`, `count`, `npc_id`, `group_id`, `locx`, `locy`, `randomx`, `randomy`, `locx1`, `locy1`, `locx2`, `locy2`, `heading`, `mapid`, `respawn_screen`, `movement_distance`, `rest`, `spawn_type`, `percentage`) VALUES (NULL, 'Great Spirit of Wind', 'Caspa', '1', '45644', '0', '32624', '32807', '0', '0', '32588', '32739', '32660', '32876', '4', '303', '1', '0', '0', '0', '100');
+
+UPDATE `dungeon` SET `new_x` = 32736, `new_y` = 32795 WHERE `src_mapid` = 4 AND `new_mapid` = 83;
