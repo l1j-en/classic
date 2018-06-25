53c3ab8a0ca1ead77c4a3bbc22db130a90a26bc3
L1J Smitty <l1jsmitty@gmail.com>
2018-06-25 00:27:28 -0400

    actually honour the ip restriction flag on the account and added SSH commands

--- src/l1j/server/ssh/command/Commands.java
+++ src/l1j/server/ssh/command/Commands.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 import l1j.server.Config;
 import l1j.server.server.Account;
@@ -306,5 +307,46 @@ class GetSiegeTimes implements Commands {
 
 	public String formatMessage(String args) {
                 return "GetSiegeTimes";
+        }
+}
+
+class ManageIpRestrictions implements Commands {
+	private static final Pattern PATTERN = Pattern.compile(
+	        "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+	
+	@Override
+	public String execute(String args) {
+		try{
+			StringTokenizer tok = new StringTokenizer(args, " ");
+			String action = tok.nextToken();
+			String account = tok.nextToken();
+			String ip = tok.nextToken();	
+			
+			if(!PATTERN.matcher(ip).matches()) {
+				return "Invalid IP!";
+			}
+			
+			Account existingAccount = Account.load(account);
+			if(existingAccount == null) {
+				return "Account doesn't exist!";
+			}
+			
+			if(action.toLowerCase().equals("add")) {
+				Account.addIpRestriction(existingAccount.getName(), ip);
+			} else {
+				Account.deleteIpRestriction(existingAccount.getName(), ip);
+			}
+			
+			return "OK";
+		} catch(Exception ex) {
+			return "iprestriction <add|del> <account> <ip>";
+		}
+	}
+
+	public String formatMessage(String args) {
+                StringTokenizer tok = new StringTokenizer(args, " ");
+                String account = tok.nextToken();
+
+                return "Add an IP to the restriction list for the account: " + account;
         }
 }
