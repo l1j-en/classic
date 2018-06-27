32024a86e6fa36009676e36f72842ecf5045e7db
L1J Smitty <l1jsmitty@gmail.com>
2018-06-26 22:28:10 -0400

    added the enable/disable of ip restriction ssh command

--- src/l1j/server/ssh/command/Commands.java
+++ src/l1j/server/ssh/command/Commands.java
@@ -350,3 +350,32 @@ class ManageIpRestrictions implements Commands {
                 return "Add an IP to the restriction list for the account: " + account;
         }
 }
+
+class ToggleIpRestriction implements Commands {
+	@Override
+	public String execute(String args) {
+		try{
+			StringTokenizer tok = new StringTokenizer(args, " ");
+			String account = tok.nextToken();
+			boolean enable = Boolean.parseBoolean(tok.nextToken());
+			
+			Account existingAccount = Account.load(account);
+			if(existingAccount == null) {
+				return "Account doesn't exist!";
+			}
+			
+			Account.enableIpRestriction(existingAccount.getName(), enable);
+			
+			return "OK";
+		} catch(Exception ex) {
+			return "enable-ip-restriction <account> <true|false>";
+		}
+	}
+
+	public String formatMessage(String args) {
+                StringTokenizer tok = new StringTokenizer(args, " ");
+                String account = tok.nextToken();
+
+                return "Add an IP to the restriction list for the account: " + account;
+        }
+}
