3e5be715735068e0a594c6f76b9526f3892d520f
tricid <tricid@l1j.org>
2018-09-19 21:09:01 -0500

    remove thread naming from C_AuthLogin, new system doesn't work that way

--- src/l1j/server/server/clientpackets/C_AuthLogin.java
+++ src/l1j/server/server/clientpackets/C_AuthLogin.java
@@ -103,7 +103,6 @@ public class C_AuthLogin extends ClientBasePacket {
 			client.setDisconnectNextClick(false); // ensure the disconnect flag isn't set when they login
 			client.sendPacket(new S_LoginResult(S_LoginResult.REASON_LOGIN_OK));
 			client.sendPacket(new S_CommonNews());
-			client.nameThread("Client_"+account.getName());
 			_log.info("Account login: account=" + account.getName()
 					+ " host=" + client.getHostname() + " Current Memory: "
 					+ SystemUtil.getUsedMemoryMB() + "MB RAM");
