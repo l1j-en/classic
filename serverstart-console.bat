f80ca10317cd519e3472c0b416963145e73deb8b
Bruce C. Miller <bm3719@gmail.com>
2019-09-07 14:01:16 -0400

    Add module build for Java9+

--- serverstart-console.bat
+++ serverstart-console.bat
@@ -1,30 +1,29 @@
-@echo off 
-title L1J-En Server Console 
-:start 
-echo Starting L1J-En Server. 
-echo. 
-REM ------------------------------------- 
-REM Default parameters for a basic server. 
-java -Xmx1024m -Xincgc -cp "l1jen.jar;lib\*" l1j.server.Server
-REM 
-REM If you have a big server and lots of memory, you could experiment for example with 
-REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts 
-REM ------------------------------------- 
-if ERRORLEVEL 2 goto restart 
-if ERRORLEVEL 1 goto error 
-goto end 
-:restart 
-echo. 
-echo Admin Restart ... 
-echo. 
-goto start 
-:error 
-echo. 
-echo Server terminated abnormaly 
-echo. 
-:end 
-echo. 
-echo server terminated 
-echo. 
-pause 
-
+@echo off
+title L1J-En Server Console
+:start
+echo Starting L1J-En Server.
+echo.
+REM -------------------------------------
+REM Default parameters for a basic server.
+java -Xmx1024m -cp "l1jen.jar;lib\*" l1j.server.Server
+REM
+REM If you have a big server and lots of memory, you could experiment for example with
+REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
+REM -------------------------------------
+if ERRORLEVEL 2 goto restart
+if ERRORLEVEL 1 goto error
+goto end
+:restart
+echo.
+echo Admin Restart ...
+echo.
+goto start
+:error
+echo.
+echo Server terminated abnormaly
+echo.
+:end
+echo.
+echo server terminated
+echo.
+pause
